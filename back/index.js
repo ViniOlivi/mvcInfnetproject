7bd29f474c2e0243ee4c3152243f587b821041c6
ViniOlivi <39277871+ViniOlivi@users.noreply.github.com>
2021-02-19 13:04:11 +0000

    validate started, no errors config yet

--- back/index.js
+++ back/index.js
@@ -2,6 +2,7 @@
 const express = require("express"); //server http
 const path = require("path");
 const generatorController = require("./controllers/generator-controller");
+const Joi = require("joi");
 const app = express();
 
 // middleware
@@ -11,6 +12,24 @@ app.use(express.static(path.join(__dirname, "public")));
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded());
 
+// Validation Joi
+const validate = (req, res, next) => {
+  // schema
+  const schema = Joi.object().keys({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+  });
+
+  const validateReturn = schema.validate(req.body);
+
+  console.log(validateReturn.error);
+
+  res.render("form", {
+    errors: validateReturn.error ? validateReturn.error.details : [],
+    body: req.body,
+  });
+};
+
 const handleRaiz = (req, res, next) => {
   res.render("index");
 };
@@ -18,8 +37,10 @@ const handleRaiz = (req, res, next) => {
 // Routes
 app.get("/", handleRaiz);
 app.get("/form", generatorController.get); //envia algo, expor uma url para acesso
-app.post("/form", generatorController.post); //recebe algo, acessar uma url para acesso
 
+app.post("/form", validate, generatorController.post); //recebe algo, acessar uma url para acesso
+
+//call server
 app.listen(3001, () => {
   console.log("servidor rolando");
   console.log("fim do index");
--- back/schemas/users.schema.js
+++
@@ -1,6 +0,0 @@
-const Joi = require("@hapi/joi");
-
-module.exports = {
-  name: Joi.string().min(3).required(),
-  email: Joi.string().email().required,
-};
